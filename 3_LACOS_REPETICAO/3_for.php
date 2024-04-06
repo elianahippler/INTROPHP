3b3e79795f02f1cfdc99192ba78052db48fdcda0
Eliana Hippler <elianahippler@gmail.com>
2024-04-05 21:14:03 -0300

    Add files via upload

---
+++ 3_LACOS_REPETICAO/3_for.php
@@ -0,0 +1,13 @@
+<?php
+    /* início do loop - condição - contador */
+
+    for($a = 1; $a <= 10; $a++){
+        $cubo = $a * $a * $a;
+        echo "O cubo de $a é $cubo <br>";
+    }
+
+    for($soma = 1; $soma <= 300; $soma++){
+        $quantos = $soma + 1;
+        echo "Fala 300! Contra $soma <br>";
+    }
+?>
