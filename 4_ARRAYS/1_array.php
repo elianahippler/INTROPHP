1fbd3e4d573cb97ff2d8b99aa010ae99ac74a13c
Eliana Hippler <elianahippler@gmail.com>
2024-04-24 22:18:26 -0300

    Add files via upload

---
+++ 4_ARRAYS/1_array.php
@@ -0,0 +1,20 @@
+<?php
+    $php = array (
+        'segunda', 
+        'terca', 
+        'quarta', 
+        'quinta', 
+        'sexta', 
+        'sabado', 
+        'domingo'
+    );
+
+    echo $php[0];
+    echo $php[1];
+    echo $php[2];
+    echo $php[3];
+    echo $php[4];
+    echo $php[5];
+    echo $php[6];
+
+?>
